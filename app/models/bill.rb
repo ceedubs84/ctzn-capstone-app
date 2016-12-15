11dbf32980e52bfbbc150299378dcf7c2356286d
Chris Woodside <christopherwoodside@gmail.com>
2016-12-14 20:26:40 -0600

    Added missing associations to Bill and Category models

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,5 +2,6 @@ class Bill < ApplicationRecord
   has_many :user_bills
   has_many :users, through: :user_bills
   has_many :comments
+  has_many :bill_categories
   has_many :categories, through: :bill_categories
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
+  has_many :bill_categories
   has_many :bills, through: :bill_categories
 end
