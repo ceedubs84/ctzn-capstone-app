5cc37ac691df140171fc945c2af7838304085f59
Chris Woodside <christopherwoodside@gmail.com>
2016-12-14 20:01:15 -0600

    Added Bill and Category associations

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,4 +2,5 @@ class Bill < ApplicationRecord
   has_many :user_bills
   has_many :users, through: :user_bills
   has_many :comments
+  has_many :categories, through: :bill_categories
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+  has_many :bills, thorugh: :bill_categories
 end
