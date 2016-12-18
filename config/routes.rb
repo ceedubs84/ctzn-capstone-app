04330328bc5e2eab77df3d55cb9ce62cc358667e
Chris Woodside <christopherwoodside@gmail.com>
2016-12-18 15:08:43 -0600

    Added Bills routes and updated User New view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'bills#index'
+  get '/bills' => 'bills#new'
+  post '/bills' => 'bills#create'
+  get '/bills/:id' => 'bills#show'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
 end
