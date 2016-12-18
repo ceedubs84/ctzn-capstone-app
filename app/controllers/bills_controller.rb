2722dd0874c5f519a0594a549d32682b746400b2
Chris Woodside <christopherwoodside@gmail.com>
2016-12-18 15:51:23 -0600

    Added Bills Index view

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,14 +1,11 @@
 class BillsController < ApplicationController
+  def index
+    @bills = Bill.all
+    render 'index.html.erb'
+  end
+
   def new
     render 'new.html.erb'
-  end
-
-  def create
-    
-  end
-
-  def show
-    
   end
 
 end
