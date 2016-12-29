5f11d6faba63091bd4141617a9bc50059a41be1f
Chris Woodside <christopherwoodside@gmail.com>
2016-12-28 20:56:44 -0600

    Added unirest API request and header

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,6 +1,11 @@
 class BillsController < ApplicationController
+  HEADERS = { "X-API-Key" => "ACnwGyW9cD70kbaj4JtPW5WypaLdKYNL4V8Coviy" }
+
   def index
-    @bills = Bill.all
+    @bills = Unirest.get(
+      "https://api.propublica.org/congress/v1/114/house/bills/introduced.json",
+      headers: HEADERS
+    ).body
     render 'index.html.erb'
   end
 
