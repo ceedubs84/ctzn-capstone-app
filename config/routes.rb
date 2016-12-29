bb0d41eaefa093bda88db0ae05b6e69ce2c9e0a0
Chris Woodside <christopherwoodside@gmail.com>
2016-12-28 18:23:47 -0600

    Added alternate Bills Index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'bills#index'
-  get '/bills' => 'bills#new'
+  get '/bills' => 'bills#index'
+  get '/bills/new' => 'bills#new'
   post '/bills' => 'bills#create'
   get '/bills/:id' => 'bills#show'
 
