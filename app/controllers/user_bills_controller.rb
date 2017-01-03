adbc2dd45c46db93cdc4e54503fc22fdf014f39d
Chris Woodside <christopherwoodside@gmail.com>
2017-01-03 16:25:21 -0600

    Updated UserBills Index action and View to display current user's bills

--- app/controllers/user_bills_controller.rb
+++ app/controllers/user_bills_controller.rb
@@ -2,10 +2,7 @@ class UserBillsController < ApplicationController
   HEADERS = { "X-API-Key" => "ACnwGyW9cD70kbaj4JtPW5WypaLdKYNL4V8Coviy" }
 
   def index
-    @data = Unirest.get(
-      "#{['https://api.propublica.org/congress/v1/114/house/bills/introduced.json']}",
-      headers: HEADERS
-    ).body
+    @user_bills = current_user.user_bills
     render 'index.html.erb'
   end
 
