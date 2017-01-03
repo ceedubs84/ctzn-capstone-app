92c4a331bee9de4800e1bb90a3a3a7cb62b4eb0c
Chris Woodside <christopherwoodside@gmail.com>
2017-01-03 16:36:09 -0600

    Added Create route for UserBills

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/bills/:id' => 'bills#show', :constraints => { :id => /[^\/]+/ }
 
   get '/user_bills' => 'user_bills#index'
+  post '/user_bills' => 'user_bills#create'
   get '/user_bills/:id' => 'user_bills#show'
 
   get '/signup' => 'users#new'
