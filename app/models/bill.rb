ad6849825701093aa3f602d2125e3b13c613069e
Chris Woodside <christopherwoodside@gmail.com>
2017-01-04 18:27:27 -0600

    Removed Bills model and controller

--- app/models/bill.rb
+++
@@ -1,8 +0,0 @@
-class Bill < ApplicationRecord
-  has_many :user_bills
-  has_many :users, through: :user_bills
-  has_many :comments
-  has_many :bill_categories
-  has_many :categories, through: :bill_categories
-  has_many :checkpoints
-end
