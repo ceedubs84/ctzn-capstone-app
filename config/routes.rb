e150c39dcc8ff105b59a45ea511a8ef440e0a48c
Chris Woodside <christopherwoodside@gmail.com>
2017-01-08 14:24:46 -0600

    Changed landing page route to UserBills Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'bills#index'
   get '/bills' => 'bills#index'
   get '/bills/new' => 'bills#new'
   post '/bills' => 'bills#create'
   get '/bills/:id' => 'bills#show', :constraints => { :id => /[^\/]+/ }
 
+  get '/' => 'user_bills#index'
   get '/user_bills' => 'user_bills#index'
   post '/user_bills' => 'user_bills#create'
   get '/user_bills/:id' => 'user_bills#show'
