e843774ceff11383bdbaaa4d4bc206dd0e7e0609
Chris Woodside <christopherwoodside@gmail.com>
2017-01-12 12:13:17 -0600

    Added trash icon to delete bills and updated flash message for Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:success] = 'Successfully logged in!'
+      flash[:success] = 'Welcome! You successfully logged in!'
       redirect_to '/'
     else
       flash[:warning] = 'Invalid email or password!'
