58016c22f2c34fb9344cd264ff8211e09d7d5a98
Chris Woodside <christopherwoodside@gmail.com>
2017-01-18 20:04:14 -0600

    Added jquery and removed bootstrap id to pass user_bill_id for creation of new CheckpointAction

--- app/assets/javascripts/checkpoint_actions.js
+++ app/assets/javascripts/checkpoint_actions.js
@@ -1,1 +1,8 @@
-// $('#myModal').modal(options);
+/* global $ */
+$(function() {
+  $('.open-modal').click(function() {
+    var billId = $(this).data().billId;
+    $("#modal-user-bill-id").val(billId);
+    $('#myModal').modal();
+  });
+});
