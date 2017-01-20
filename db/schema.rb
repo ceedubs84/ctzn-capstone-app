c0b07f103caeb8f623e63073b66566199f09b5f7
Chris Woodside <christopherwoodside@gmail.com>
2017-01-20 17:09:45 -0600

    Added Legislator model, Legislators controller, and migrated to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118022252) do
+ActiveRecord::Schema.define(version: 20170120230354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,11 @@ ActiveRecord::Schema.define(version: 20170118022252) do
     t.text     "text"
     t.integer  "user_id"
     t.integer  "bill_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "legislators", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
