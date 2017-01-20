611d35149ca160142c11c366a3ed4d01fd2b77c1
Chris Woodside <christopherwoodside@gmail.com>
2017-01-19 20:22:46 -0600

    Added Show action for API/V1 UserBills

--- app/controllers/api/v1/user_bills_controller.rb
+++ app/controllers/api/v1/user_bills_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::UserBillsController < ApplicationController
     @user_bills = current_user.user_bills
     render 'index.json.jbuilder'
   end
+
+  def show
+    @user_bill = UserBill.find_by(id: params[:id])
+    render 'show.json.jbuilder'
+  end
 end
