a7b2d67a92e779d9fd23459ed6f28bffd9a4c498
Chris Woodside <christopherwoodside@gmail.com>
2017-01-19 20:45:59 -0600

    Added api/v1 CheckpointAction Show action

--- app/controllers/api/v1/checkpoint_actions_controller.rb
+++ app/controllers/api/v1/checkpoint_actions_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::CheckpointActionsController < ApplicationController
     @checkpoint_actions = current_user.checkpoint_actions
     render 'index.json.jbuilder'
   end
+
+  def show
+    @checkpoint_action = CheckpointAction.find_by(id: params[:id])
+    render 'show.json.jbuilder'
+  end
 end
