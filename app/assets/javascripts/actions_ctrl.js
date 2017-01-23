4313275a4b77953e930942a0f2a8ae9c01cab25d
Chris Woodside <christopherwoodside@gmail.com>
2017-01-23 10:38:32 -0600

    Updated action controller for ActionsCtrl and added new API for Bills controller

--- app/assets/javascripts/actions_ctrl.js
+++ app/assets/javascripts/actions_ctrl.js
@@ -2,18 +2,18 @@
 
 (function() {
   angular.module("app").controller("actionsCtrl", function($scope, $http) {
-    $http.get('http://localhost:3000/api/v1/checkpoint_actions').then(function(response) {
-      $scope.checkpointActions = response.data;
-      $scope.orderAttribute = 'date';
-      $scope.isOrderDescending = true;
-    });
-
-    $scope.toggleComplete = function(status) {
-      status.complete = !status.complete;
-      console.log('status');
+    $scope.setup = function() {
+      $http.get('/api/v1/checkpoint_actions').then(function(response) {
+        $scope.checkpointActions = response.data;
+        $scope.orderAttribute = 'date';
+        $scope.isOrderDescending = true;
+      });
+    };
+    $scope.toggleComplete = function(inputStatus) {
+      inputStatus.complete = !inputStatus.complete;
     };
 
-    $scope.addCheckpointAction = function(inputActionId, inputDescription, inputDate, inputUserBillId)
+    $scope.addCheckpointAction = function(inputActionId, inputDescription, inputDate, inputUserBillId) {
       var params = {
         action_id: inputActionId,
         description: inputDescription,
@@ -26,6 +26,34 @@
       }, function(error) {
         $scope.errors = error.data.errors;
       });
+    };
+
+    $scope.deleteCheckpointAction = function(inputCheckpointAction) {
+      var index = $scope.checkpointActions.indexOf(inputCheckpointAction);
+      if (index !== -1) {
+        $scope.checkpointActions.splice(index, 1);
+      }
+    };
+
+    $scope.changeOrderAttribute = function(inputAttribute) {
+      if (inputAttribute === $scope.orderAttribute) {
+        $scope.isOrderDescending = !$scope.isOrderDescending;
+      } else {
+        $scope.isOrderDescending = false;
+      }
+      $scope.orderAttribute = inputAttribute;
+    };
+
+    $scope.getSortIcon = function(inputAttribute) {
+      if (inputAttribute === $scope.orderAttribute) {
+        if ($scope.isOrderDescending) {
+          return '\u2193';
+        } else {
+          return '\u2191';
+        }
+      } else {
+        return '';
+      }
     };
     
     window.$scope = $scope;
--- app/controllers/api/v1/checkpoint_actions_controller.rb
+++ app/controllers/api/v1/checkpoint_actions_controller.rb
@@ -4,8 +4,26 @@ class Api::V1::CheckpointActionsController < ApplicationController
     render 'index.json.jbuilder'
   end
 
+  def create
+    @checkpoint_action = CheckpointAction.new(
+      action_id: params[:action_id],
+      description: params[:description],
+      date: params[:date],
+      user_bill_id: params[:user_bill_id],
+      user_id: current_user.id,
+      status: "Incomplete"
+    )
+    if checkpoint_action.save
+      flash[:success] = 'Action created successfully!'
+      redirect_to "/checkpoint_actions/#{params[:user_bill_id]}"
+    else
+      flash[:warning] = 'Action not saved, please try again.'
+      redirect_to '/checkpoint_actions'
+    end
+  end
+
   def show
-    @checkpoint_action = CheckpointAction.find_by(id: params[:id])
+    @checkpoint_action = CheckpointAction.where(user_bill_id: params[:id])
     render 'show.json.jbuilder'
   end
 end
