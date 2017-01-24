5f3393f13ffab58c3810d327bbb04ff51ab3f422
Chris Woodside <christopherwoodside@gmail.com>
2017-01-23 19:03:39 -0600

    Blocked out legislators_crtl.js

--- app/assets/javascripts/legislators_ctrl.js
+++ app/assets/javascripts/legislators_ctrl.js
@@ -1,25 +1,25 @@
 /* global angular */
 
-(function() {
-  angular.module("app").controller("legislatorsCtrl", function($scope, $http) {
-    $http.get('http://localhost:3000/api/v1/legislators').then(function(response) {
-      $scope.legislators = response.data;
-      $scope.orderAttribute = 'lastName';
-      $scope.isOrderDescending = true;
-    });
+// (function() {
+//   angular.module("app").controller("legislatorsCtrl", function($scope, $http) {
+//     $http.get('http://localhost:3000/api/v1/legislators').then(function(response) {
+//       $scope.legislators = response.data;
+//       $scope.orderAttribute = 'lastName';
+//       $scope.isOrderDescending = true;
+//     });
 
-    $scope.addLegislator = function(inputBioguideId)
-      var params = {
-        bioguide_id: inputBioguideId,
-        user_id: currentUserId,
-      };
-      $http.post('/api/v1/legislators', params).then(function(response) {
-        $scope.legislators.push(response.data);
-      }, function(error) {
-        $scope.errors = error.data.errors;
-      });
-    };
+//     $scope.addLegislator = function(inputBioguideId)
+//       var params = {
+//         bioguide_id: inputBioguideId,
+//         user_id: currentUserId,
+//       };
+//       $http.post('/api/v1/legislators', params).then(function(response) {
+//         $scope.legislators.push(response.data);
+//       }, function(error) {
+//         $scope.errors = error.data.errors;
+//       });
+//     };
     
-    window.$scope = $scope;
-  });
-})();
+//     window.$scope = $scope;
+//   });
+// })();
