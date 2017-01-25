f70d2d765e448cd9317de8634612dea87bc7b6c5
Chris Woodside <christopherwoodside@gmail.com>
2017-01-25 10:33:01 -0600

    Updated JS for circles in Application HTML and in circles-master.js

--- public/assets/js/plugins/circles-master.js
+++ public/assets/js/plugins/circles-master.js
@@ -6,109 +6,109 @@ var CirclesMaster = function() {
     initCirclesMaster1: function() {
 				//Circles 1
       Circles.create({
-        id:         'circle-1',
+        id: 'circle-1',
         percentage: 87,
-        radius:     80,
-        width:      8,
-        number:     87,
-        text:       '%',
-        colors:     ['#eee', '#72c02c'],
-        duration:   2000
+        radius: 80,
+        width: 8,
+        number: 87,
+        text: '%',
+        colors: ['#eee', '#72c02c'],
+        duration: 2000
       });
 
 				//Circles 2
       Circles.create({
-        id:         'circle-2',
+        id: 'circle-2',
         percentage: 74,
-        radius:     80,
-        width:      8,
-        number:     74,
-        text:       '%',
-        colors:     ['#eee', '#72c02c'],
-        duration:   2000
+        radius: 80,
+        width: 8,
+        number: 74,
+        text: '%',
+        colors: ['#eee', '#72c02c'],
+        duration: 2000
       });
 
         //Circles 3
       Circles.create({
-        id:         'circle-3',
+        id: 'circle-3',
         percentage: 65,
-        radius:     80,
-        width:      8,
-        number:     65,
-        text:       '%',
-        colors:     ['#eee', '#72c02c'],
-        duration:   2000
+        radius: 80,
+        width: 8,
+        number: 65,
+        text: '%',
+        colors: ['#eee', '#72c02c'],
+        duration: 2000
       });
 
           //Circles 4
       Circles.create({
-        id:         'circle-4',
+        id: 'circle-4',
         percentage: 69,
-        radius:     35,
-        width:      2,
-        number:     69,
-        text:       '%',
-        colors:     ['#eee', '#72c02c'],
-        duration:   2000
+        radius: 35,
+        width: 2,
+        number: 69,
+        text: '%',
+        colors: ['#eee', '#72c02c'],
+        duration: 2000
       });
 
             //Circles 5
       Circles.create({
-        id:         'circle-5',
+        id: 'circle-5',
         percentage: 82,
-        radius:     35,
-        width:      2,
-        number:     82,
-        text:       '%',
-        colors:     ['#eee', '#9B6BCC'],
-        duration:   2000
+        radius: 35,
+        width: 2,
+        number: 82,
+        text: '%',
+        colors: ['#eee', '#9B6BCC'],
+        duration: 2000
       });
 
             //Circles 6
       Circles.create({
-        id:         'circle-6',
+        id: 'circle-6',
         percentage: 87,
-        radius:     80,
-        width:      3,
-        number:     87,
-        text:       '%',
-        colors:     ['#eee', '#72c02c'],
-        duration:   2000
+        radius: 80,
+        width: 3,
+        number: 87,
+        text: '%',
+        colors: ['#eee', '#72c02c'],
+        duration: 2000
       });
 
            //Circles 7
       Circles.create({
-        id:         'circle-7',
+        id: 'circle-7',
         percentage: 74,
-        radius:     80,
-        width:      3,
-        number:     74,
-        text:       '%',
-        colors:     ['#eee', '#72c02c'],
-        duration:   2000
+        radius: 80,
+        width: 3,
+        number: 74,
+        text: '%',
+        colors: ['#eee', '#72c02c'],
+        duration: 2000
       });
 
            //Circles 8
       Circles.create({
-        id:         'circle-8',
+        id: 'circle-8',
         percentage: 65,
-        radius:     80,
-        width:      3,
-        number:     65,
-        text:       '%',
-        colors:     ['#eee', '#72c02c'],
-        duration:   2000
+        radius: 80,
+        width: 3,
+        number: 65,
+        text: '%',
+        colors: ['#eee', '#72c02c'],
+        duration: 2000
       });
           //Circles 9
       Circles.create({
-        id:         'circle-9',
+        id: 'circle-9',
         percentage: 91,
-        radius:     80,
-        width:      3,
-        number:     91,
-        text:       '%',
-        colors:     ['#eee', '#72c02c'],
-        duration:   2000
+        radius: 80,
+        width: 3,
+        number: 91,
+        text: '%',
+        colors: ['#eee', '#72c02c'],
+        duration: 2000
       });
     },
         
@@ -123,14 +123,14 @@ var CirclesMaster = function() {
           percentage = 45 + (i * 9);
 
         Circles.create({
-          id:         child.id,
+          id: child.id,
           percentage: percentage,
-          radius:     70,
-          width:      2,
-          number:     percentage / 1,
-          text:       '%',
-          colors:     colors[i - 1],
-          duration:   2000,
+          radius: 70,
+          width: 2,
+          number: percentage / 1,
+          text: '%',
+          colors: colors[i - 1],
+          duration: 2000,
         });
       }
     }
