1e401c944a30cac4deecaf2a737c4b991f6f1443
Chris Woodside <christopherwoodside@gmail.com>
2017-01-26 13:29:26 -0600

    Minor updates testing

--- app/models/user_bill.rb
+++ app/models/user_bill.rb
@@ -1,10 +1,11 @@
 class UserBill < ApplicationRecord
   belongs_to :user
   has_many :checkpoint_actions
+  has_many :actions, through: :checkpoint_actions
 
   def progress_percent
     num_checkpoint_actions = checkpoint_actions.count # total number
-    num_completed = checkpoint_actions.where(status: "complete").count
+    num_completed = checkpoint_actions.where(status: "Complete").count
     return (num_completed / num_checkpoint_actions.to_f) * 100
   end
 end
--- app/views/api/v1/checkpoint_actions/show.json.jbuilder
+++ app/views/api/v1/checkpoint_actions/show.json.jbuilder
@@ -1,3 +1,4 @@
+json.id @checkpoint_action.id
 json.action_id @checkpoint_action.action_id
 json.status @checkpoint_action.status
 json.user_bill_id @checkpoint_action.user_bill_id
